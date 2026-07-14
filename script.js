af4e2674377f3ee20ed2325eaed22eaee58caf5f
anas-virtual <anas.ahmed2@education.nsw.gov.au>
2026-07-14 23:10:42 +1000

    Progress on the about.html,javascript, css, index.html
    I made good progress on the css and java. I believe I dont need to add anything else on my index.html and I been working on my About page too like adding some information about the musallah.

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-document.addEventListener('DOMContentLoaded', () =>{
-    const menu = document.que
-}
-)
+document.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu-btn');
+    const links = document.querySelector('.nav-links');
+
+    if (menu) {
+        menu.addEventListener('click', () => {
+            const open = links.classList.toggle('open');
+        });
+    }
+
+    const top = document.querySelector('.back-top');
+    
+})
